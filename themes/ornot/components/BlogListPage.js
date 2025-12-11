1d606b38673039edc5bde960b659c149e23a2665
Jinsheng Chen <158833684+ornotChen@users.noreply.github.com>
2025-12-12 00:17:29 +0900

    Update BlogListPage.js

--- themes/ornot/components/BlogListPage.js
+++ themes/ornot/components/BlogListPage.js
@@ -50,7 +50,7 @@ export const BlogListPage = props => {
     <div className='w-full'>
       <div
         id='posts-wrapper'
-        className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 px-6 md:px-12 lg:px-24 py-12'>
+        className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 px-0 md:px-4 lg:px-0 lg:mx-[5%] py-12'>
         {posts?.map((post, index) => (
           <BlogPost
             index={index}
