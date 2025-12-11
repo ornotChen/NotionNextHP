d38325ce2f23af5d985593d27d0e12cb860f2a0d
Jinsheng Chen <158833684+ornotChen@users.noreply.github.com>
2025-12-12 01:35:22 +0900

    Update MenuItemDrop.js

--- themes/ornot/components/MenuItemDrop.js
+++ themes/ornot/components/MenuItemDrop.js
@@ -33,12 +33,12 @@ export const MenuItemDrop = ({ link }) => {
       {/* 子菜单 */}
       {hasSubMenu && (
         <ul
-          className={`${show ? 'visible opacity-100 bottom-16 ' : 'invisible opacity-0 bottom-14'} border-gray-100  bg-white rounded-lg overflow-hidden  dark:bg-black dark:border-gray-800 bg-opacity-60 transition-all duration-300 z-20 absolute block drop-shadow-lg `}>
+          className={`${show ? 'visible opacity-100 top-16 ' : 'invisible opacity-0 top-14'} bg-[#f5f5f4]/90 dark:bg-[#111111]/90 backdrop-blur-md rounded-lg shadow-lg overflow-hidden transition-all duration-300 z-20 absolute block drop-shadow-lg `}>
           {link.subMenus.map((sLink, index) => {
             return (
               <li
                 key={index}
-                className='text-gray-700 dark:text-gray-200  hover:bg-gray-50 dark:hover:bg-gray-900 tracking-widest transition-all duration-200  dark:border-gray-800 py-3 pr-6 pl-3'>
+                className='text-gray-700 dark:text-gray-200  hover:bg-gray-100/70 dark:hover:bg-gray-800/70 tracking-widest font-light transition-all duration-200 dark:border-gray-800 py-3 pr-6 pl-3'>
                 <Link href={sLink.href} target={link?.target}>
                   <span className='text-sm text-nowrap font-extralight'>
                     {link?.icon && <i className={sLink?.icon}> &nbsp; </i>}
